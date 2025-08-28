abaf63643315e0202cf5aa58e8117560ea315c26
gksdud1109 <vectorh532@gmail.com>
2025-08-28 17:34:01 +0900

    apprunner 추가

--- src/main/java/com/back/PersonService.java
+++ src/main/java/com/back/PersonService.java
@@ -1,11 +1,19 @@
 package com.back;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
 
+    private final PersonRepository personRepository;
+
+    public PersonService(@Qualifier("personRepository") PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     public int count(){
-        return 3;
+        return personRepository.count();
     }
 }
